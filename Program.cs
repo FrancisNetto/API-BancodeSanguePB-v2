4e667fbe1e0f9c5d0f0c04da581e39a765287d84
Francisco Geovan Queiroga de Assis Netto <102336789+FrancisNetto@users.noreply.github.com>
2022-11-19 09:41:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using BancodeSangue.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionStringMysql = builder.Configuration.GetConnectionString("ConnectionMysql");
+builder.Services.AddDbContext<SangueDbContent>(options => options.UseMySql(
+    connectionStringMysql,
+    ServerVersion.Parse("8.0.31")
+    )
+);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
